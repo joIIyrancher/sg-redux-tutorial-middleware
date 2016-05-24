84feaf43a08abc4f08ed47a6557b2cd1a52086d2
Johnny Hsiao <johnnyhsiao87@gmail.com>
2016-05-24 13:39:50 -0400

    middleware finish video 52 - pre-middleware

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Component } from 'react';
+import UserList from '../containers/user_list';
 
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+        <UserList />
+      </div>
     );
   }
 }
